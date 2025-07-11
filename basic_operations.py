dec984bcfa442a8653586ec5a25c9582a48d03cb
frederick <muhirefrederick@gmail.com>
2025-07-11 04:21:10 +0200

    The basic_operations.py has been updated

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+# this is my first python project
+number = 10
+number2 = 5
+
+print("Addition of 10 and 5 is", number + number2) # Addition of 10 and 5 is 15
+print("Subtraction of 10 and 5 is", number - number2) # Subtraction of 10 and 5 is 5
+print("Multiplication of 10 and 5 is", number * number2) # Multiplication of 10 and 5# is 50
+
