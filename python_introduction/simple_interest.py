d2eb3d09e8bd3ee36bd5ccedf05ce1cb21961465
frederick <muhirefrederick@gmail.com>
2025-07-11 05:51:37 +0200

    Updated

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,4 @@
-#simple interesIt calculator
-R = 0.05
-P = 1000
-T = 3
-I = P * R * T
-print("The simple interest is:", I)
+principal = 1000
+rate = 0.05
+time = 3
+print("The simple interest is:", principal * rate * time)
