e1f8f0cf99668cd4375eebdf3f3588af438db2b6
frederick <muhirefrederick@gmail.com>
2025-08-10 20:13:09 +0200

    Boo file updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,12 +1,20 @@
+# book_class.py
+
 class book:
     def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance."""
         self.title = title
         self.author = author
         self.year = year
 
+    def __del__(self):
+        """Destructor that prints a message when the object is deleted."""
+        print(f"Deleting {self.title}")
+
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.year})"
+        """Human-readable string representation."""
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"book(title={self.title!r}, author={self.author!r}, year={self.year!r})"
-    
+        """Official string representation that can recreate the object."""
+        return f"book('{self.title}', '{self.author}', {self.year})"
